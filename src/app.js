97fb1c133ed61baf4caf50768552467585d39be3
mirohi <m_hintikka@hotmail.com>
2024-04-13 16:09:07 +0300

    add assignment 6 all tasks done

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import api from "./api/index.js";
 import authRouter from "./api/routes/auth-router.js";
 import { errorHandler, notFoundHandler } from "./middlewares.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static("public"));
@@ -12,7 +14,7 @@ app.use("/api/v1", api);
 app.use("/api/auth", authRouter);
 app.use(notFoundHandler);
 app.use(errorHandler);
-//app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
